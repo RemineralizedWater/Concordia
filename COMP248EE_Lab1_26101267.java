67c3c0528dd776e256c5efde13076cdda4cff026
RemineralizedWater <31147432+RemineralizedWater@users.noreply.github.com>
2017-09-30 21:58:47 -0400

    Lab1

---
+++ COMP248EE_Lab1_26101267.java
@@ -0,0 +1,19 @@
+/* ------------------------
+   Lab 1
+   Name & Student ID: Michael Rowe 26101267
+   COMP248EE
+ */
+
+// This is the class for Lab 1
+
+public class COMP248EE_Lab1_26101267 {
+	
+	// This is the main method for Lab 1
+
+	public static void main(String[] args) {
+		
+		// The following line of code prints a statement.
+		System.out.println("Hello world!");
+	}
+
+}
